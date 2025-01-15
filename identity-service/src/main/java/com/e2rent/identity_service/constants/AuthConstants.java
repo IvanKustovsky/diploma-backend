4aaffbb12fc2a0a3267baed162ab10848f7c1a2e
IvanKustovsky <vanyakustovsky@gmail.com>
2025-01-15 11:36:57 +0200

    Deleted identity-service folder.

--- identity-service/src/main/java/com/e2rent/identity_service/constants/AuthConstants.java
+++
@@ -1,9 +0,0 @@
-package com.e2rent.identity_service.constants;
-
-public class AuthConstants {
-
-    private AuthConstants() {}
-
-    public static final String STATUS_200 = "200";
-    public static final String MESSAGE_200 = "Request processed successfully";
-}
