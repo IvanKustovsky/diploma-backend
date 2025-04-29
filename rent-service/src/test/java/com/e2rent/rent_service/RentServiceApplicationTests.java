6d7275311477ca56add8546a78d1f6ec109e1edd
IvanKustovsky <vanyakustovsky@gmail.com>
2025-04-29 14:56:28 +0300

    Initial commit for rent-service

---
+++ rent-service/src/test/java/com/e2rent/rent_service/RentServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.e2rent.rent_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RentServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
