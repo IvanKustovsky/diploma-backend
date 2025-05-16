cb09edda170a2eddfb475dacce5a23d7f2db8df4
IvanKustovsky <vanyakustovsky@gmail.com>
2025-05-16 14:59:41 +0300

    Added "OTHER" subcategory for equipment

--- equipment-service/src/main/java/com/e2rent/equipment/mapper/CategoryMapper.java
+++ equipment-service/src/main/java/com/e2rent/equipment/mapper/CategoryMapper.java
@@ -12,25 +12,25 @@ public class CategoryMapper {
     public static List<EquipmentSubcategory> getSubcategoriesFor(EquipmentCategory category) {
         return switch (category) {
             case BACKUP_POWER -> List.of(
-                    DIESEL_GENERATOR, GAS_GENERATOR, INVERTER_GENERATOR, UPS, ENERGY_STORAGE
+                    DIESEL_GENERATOR, GAS_GENERATOR, INVERTER_GENERATOR, UPS, ENERGY_STORAGE, OTHER
             );
             case RENEWABLE_ENERGY -> List.of(
-                    SOLAR_PANEL, WIND_TURBINE, SOLAR_INVERTER, CHARGE_CONTROLLER
+                    SOLAR_PANEL, WIND_TURBINE, SOLAR_INVERTER, CHARGE_CONTROLLER, OTHER
             );
             case DISTRIBUTION -> List.of(
-                    DISTRIBUTION_BOX, CIRCUIT_BREAKER, PHASE_SWITCH, RELAY
+                    DISTRIBUTION_BOX, CIRCUIT_BREAKER, PHASE_SWITCH, RELAY, OTHER
             );
             case CABLING -> List.of(
-                    EXTENSION_CORD, CABLE_DRUM, ADAPTER, GROUNDING_EQUIPMENT
+                    EXTENSION_CORD, CABLE_DRUM, ADAPTER, GROUNDING_EQUIPMENT, OTHER
             );
             case INDUSTRIAL -> List.of(
-                    TRANSFORMER, VOLTAGE_STABILIZER, FREQUENCY_CONVERTER, ELECTRIC_COMPRESSOR
+                    TRANSFORMER, VOLTAGE_STABILIZER, FREQUENCY_CONVERTER, ELECTRIC_COMPRESSOR, OTHER
             );
             case TOOLS -> List.of(
-                    CHARGING_STATION, WELDING_MACHINE, DC_TO_AC_INVERTER
+                    CHARGING_STATION, WELDING_MACHINE, DC_TO_AC_INVERTER, OTHER
             );
             case MONITORING -> List.of(
-                    ENERGY_METER, SMART_SENSOR, SMART_HOME_DEVICE
+                    ENERGY_METER, SMART_SENSOR, SMART_HOME_DEVICE, OTHER
             );
             case OTHER -> List.of(OTHER);
         };
